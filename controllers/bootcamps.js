9534eaea774c44dbb0826e25786b0dc363a3df4e
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-02-14 11:59:18 +0100

    Install morgan package

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,10 +2,8 @@
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: 'Show all bootcamps' });
-}
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+};
 
 // @desc      Get single bootcamps
 // @route     GET /api/v1/bootcamps/:id
@@ -14,16 +12,14 @@ exports.getBootcamp = (req, res, next) => {
   res
     .status(200)
     .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-}
+};
 
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Private
 exports.createBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: 'Create new bootcamp' });
-}
+  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+};
 
 // @desc      Update bootcamp
 // @route     PUT /api/v1/bootcamps/:id
@@ -32,7 +28,7 @@ exports.updateBootcamp = (req, res, next) => {
   res
     .status(200)
     .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-}
+};
 
 // @desc      Delete bootcamp
 // @route     DELETE /api/v1/bootcamps
@@ -41,4 +37,4 @@ exports.deleteBootcamp = (req, res, next) => {
   res
     .status(204)
     .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-}
+};
