f0547a9d590af562349e11406dc63799e8b818b1
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-02-22 13:27:40 +0100

    Create course model and seeder

--- seeder.js
+++ seeder.js
@@ -7,7 +7,8 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 
 // Load models
-const bootcamp = require('./models/Bootcamp');
+const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,10 +23,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 // IMport into DB
 const importData = async () => {
   try {
-    await bootcamp.create(bootcamps);
+    await Bootcamp.create(bootcamps);
+    await Course.create(courses);
 
     console.log('Data Imported...'.green.inverse);
     process.exit();
@@ -37,7 +43,8 @@ const importData = async () => {
 // Delete data
 const deleteData = async () => {
   try {
-    await bootcamp.deleteMany();
+    await Bootcamp.deleteMany();
+    await Course.deleteMany();
 
     console.log('Data Destroyed...'.red.inverse);
     process.exit();
