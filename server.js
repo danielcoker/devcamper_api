bf24233ea23cb1f0996af812d90bca3bc7acee25
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-02-28 14:16:30 +0100

    Get all reviews

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ connectDB();
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
+const reviews = require('./routes/reviews');
 
 const app = express();
 
@@ -41,7 +42,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
-app.use('/api/v1/auth', auth);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/reviews', reviews);
 
 app.use(errorHandler);
 
