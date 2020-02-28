b3e4b2ae491c29436fc0fb2d15fd5ff1fcf146c1
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-02-28 14:29:34 +0100

    Add get single review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {
-  getReviews
-  // getReview,
+  getReviews,
+  getReview
   // addReview,
   // updateReview,
   // deleteReview
@@ -23,9 +23,7 @@ router.route('/').get(
 );
 // .post(protect, authorize('user', 'admin'), addReview);
 
-// router
-//   .route('/:id')
-//   .get(getReview)
+router.route('/:id').get(getReview);
 //   .put(protect, authorize('user', 'admin'), updateReview)
 //   .delete(protect, authorize('user', 'admin'), deleteReview);
 
