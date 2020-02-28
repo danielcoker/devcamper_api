dbb03fb6d737fec7a4d432a8b830734eea82a0c5
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-02-28 14:43:11 +0100

    Add review to bootcamp

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const {
   getReviews,
-  getReview
-  // addReview,
+  getReview,
+  addReview
   // updateReview,
   // deleteReview
 } = require('../controllers/reviews');
@@ -14,14 +14,16 @@ const router = express.Router({ mergeParams: true });
 const advancedResults = require('../middlewares/advancedResults');
 const { protect, authorize } = require('../middlewares/auth');
 
-router.route('/').get(
-  advancedResults(Review, {
-    path: 'bootcamp',
-    select: 'name description'
-  }),
-  getReviews
-);
-// .post(protect, authorize('user', 'admin'), addReview);
+router
+  .route('/')
+  .get(
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: 'name description'
+    }),
+    getReviews
+  )
+  .post(protect, authorize('user', 'admin'), addReview);
 
 router.route('/:id').get(getReview);
 //   .put(protect, authorize('user', 'admin'), updateReview)
--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
-app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 app.use('/api/v1/reviews', reviews);
 
 app.use(errorHandler);
